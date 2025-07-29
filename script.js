00aa262956d4e89c8b6a4ca0181f9183b63ff388
Mary81512 <218373560+Mary81512@users.noreply.github.com>
2025-07-29 17:41:50 +0200

    added painting effect and button

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-const grid16 = ((div) => {
-    for (let i=0 ; i < 16 ; i++) {
+const header = document.createElement("div");
+const button = document.createElement("button");
+button.textContent = "gridsize";
+button.style.cssText = "font-size: 20px";
+document.body.insertBefore(header, container);
+header.appendChild(button);
+
+const grid16 = (() => {
+    for (let row=0 ; row < 16 ; row++) {
         const gridDivRow = document.createElement("div");
         gridDivRow.classList.add("gridRow");
-        container.appendChild(gridDivRows);
+        container.appendChild(gridDivRow);
 
-        for (let i=0 ; i < 16 ; i++) {
+        for (let col=0 ; col < 16 ; col++) {
             const gridDivItem = document.createElement("div");
             gridDivItem.classList.add("gridItem");
-            gridDivItem.style.cssText = "border: 2px solid black; width: 50px; height: 50px; background: bisque";
-            gridDiv.appendChild(gridDivItem);
+            gridDivItem.style.cssText = "width: 50px; height: 50px; background: bisque";
+            gridDivItem.addEventListener('mouseenter', () => {
+                gridDivItem.style.backgroundColor = 'black';
+            });
+            gridDivRow.appendChild(gridDivItem);
+        
         }; 
     };
+    
 });
 
 grid16();
+
+container.addEventListener("click", () => {
+  const allItems = document.querySelectorAll(".gridItem");
+  allItems.forEach(item => {
+    item.style.backgroundColor = "bisque";
+  });
+});
+
